266e5c1381d31c77ab1fec34512a024ef5eac99a
Eric-Souza <eric.bh18souza@gmail.com>
2022-03-25 10:14:20 -0300

    feat: :sparkles: users search developed

--- src/contexts/github/GithubContext.js
+++ src/contexts/github/GithubContext.js
@@ -27,11 +27,42 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+  // Search user results
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
+
+    const { items } = await response.json();
+
+    dispatch({
+      type: 'GET_USERS',
+      payload: items,
+    });
+  };
+
+  // Clear users from state
+  const clearUsers = () =>
+    dispatch({
+      type: 'CLEAR_USERS',
+    });
+
+  // Trigger loading component
   const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        fetchUsers,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
